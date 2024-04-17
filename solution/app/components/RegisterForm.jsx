33c9ce6824f5a336c2721b0f0d72bb9cd9084fed
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-04-17 10:13:41 -0700

    add: registration form

--- solution/app/components/RegisterForm.jsx
+++ solution/app/components/RegisterForm.jsx
@@ -1,18 +1,100 @@
-import Link from 'next/link'
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function RegisterForm() {
+  const [screenName, setScreenName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!screenName || !email || !password) {
+      setError("All fields are necessary.");
+      return;
+    }
+
+    try {
+      const userExists = await fetch("api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      const { user } = await userExists.json();
+
+      if (user) {
+        setError("User already exists.");
+        return;
+      }
+
+      const res = await fetch("api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          screenName,
+          email,
+          password,
+        }),
+      });
+
+      if (res.ok) {
+        const form = e.target;
+        form.reset();
+        router.push("/dashboard");
+      } else {
+        console.log("User registration failed.");
+      }
+    } catch (error) {
+      console.log("Error during registration: ", error);
+    }
+  };
+
   return (
-    <>
-      <div>
-        <h1>Create an account</h1> 
-        <form>
-          <input type='text'/>
-          <button type='submit'>Play Now</button>
+    <div className="grid place-items-center h-screen">
+      <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
+        <h1 className="text-xl font-bold my-4">Register</h1>
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+          <input
+            onChange={(e) => setScreenName(e.target.value)}
+            type="text"
+            placeholder="Username"
+          />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+          />
+          <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
+            Register
+          </button>
+
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
+
+          <Link className="text-sm mt-3 text-right" href={"/login"}>
+            Already have an account? <span className="underline">Login</span>
+          </Link>
         </form>
-        <div className='flex flex-col'>
-          <Link href='/login'>Sign In</Link>
-          <Link href='/'>Home</Link>
-        </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
