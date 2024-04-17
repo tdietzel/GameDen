7a6af0b03431dcbe055d9cd47aaad7edbf205e64
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-04-17 09:43:17 -0700

    add: dashboard, login & register pages

--- solution/app/login/page.jsx
+++ solution/app/login/page.jsx
@@ -1,11 +1,11 @@
 import LoginForm from "../components/LoginForm";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "../pages/[...nextauth]/route";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function Login() {
   const session = await getServerSession(authOptions);
 
-  if (session) redirect("/");
+  if (session) redirect("/dashboard");
   return <LoginForm />;
 }
--- solution/app/register/page.jsx
+++ solution/app/register/page.jsx
@@ -1,11 +1,11 @@
 import RegisterForm from "../components/RegisterForm";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "../pages/[...nextauth]/route";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function Register() {
   const session = await getServerSession(authOptions);
 
-  if (session) redirect("/");
+  if (session) redirect("/dashboard");
   return <RegisterForm />;
 }
