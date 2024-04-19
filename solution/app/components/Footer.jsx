cb9c9b034432e3c20b997bc03f0f8bcf81ea4ef7
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-04-19 02:58:13 -0700

    add: basic footer text

---
+++ solution/app/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+      <div className="text-center text-white bg-gray-800">
+        <p>© 2024 Game Den | All Rights Reserved.</p>
+      </div>
+    </>
+  )
+}
