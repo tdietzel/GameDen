20bb63f826dabb44293614a9aa145260bae6f688
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-04-23 16:41:32 -0700

    add: error handling for status 'loading'

--- solution/app/pages/dashboard/page.jsx
+++ solution/app/pages/dashboard/page.jsx
@@ -5,8 +5,12 @@ import Header from '../../components/Header'
 import ChatDashboard from '../../components/ChatDashboard'
 
 export default function Dashboard() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   const router = useRouter();
+
+  if (status === 'loading') {
+    return <p>Loading...</p>;
+  }
 
   if (!session) {
     router.replace('/');
