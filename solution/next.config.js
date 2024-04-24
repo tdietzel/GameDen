2e8a7d4620c7ad2f2a39367571c473b152904e74
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-04-24 07:55:27 -0700

    update: next config

--- solution/next.config.js
+++ solution/next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: false,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+    return config;
+  },
+};
