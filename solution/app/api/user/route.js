ae0fdaa5d24c8eb70a1f0c380155294d5bdcfcd2
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-04-24 19:43:51 -0700

    update: user PUT request to change stats based on win/lose

--- solution/app/api/user/route.js
+++ solution/app/api/user/route.js
@@ -66,36 +66,108 @@ export const GET = async (req) => {
   }
 };
 
+// export const PUT = async (req) => {
+//   try {
+//     const session = await getServerSession(req)
+//     if (!session) {
+//       return new NextResponse(
+//         JSON.stringify({ message: 'Unauthorized' }),
+//         { status: 401, headers: { 'Content-Type': 'application/json' } }
+//       )
+//     }
+
+//     await connectMongoDB()
+//     const user = await UserModel.findOne({ email: session.user.email })
+
+//     if (user.coins > user.maxCoins) {
+//       user.maxCoins = user.coins
+//       await user.save()
+//     }
+
+//     return new NextResponse(
+//       JSON.stringify({ message: 'Maximum coins updated' }),
+//       { status: 200, headers: { 'Content-Type': 'application/json' } }
+//     )
+//   } catch (error) {
+//     console.error(error)
+//     return new NextResponse(
+//       JSON.stringify({ message: 'Internal Server Error' }),
+//       { status: 500, headers: { 'Content-Type': 'application/json' } }
+//     )
+//   }
+// }
+
+
 export const PUT = async (req) => {
   try {
-    const session = await getServerSession(req)
+    const session = await getServerSession(req);
     if (!session) {
       return new NextResponse(
         JSON.stringify({ message: 'Unauthorized' }),
         { status: 401, headers: { 'Content-Type': 'application/json' } }
-      )
+      );
     }
 
-    await connectMongoDB()
-    const user = await UserModel.findOne({ email: session.user.email })
+    const { action } = await req.json();
+
+    await connectMongoDB();
+    const user = await UserModel.findOne({ email: session.user.email });
+
+    if (action === 'blackjackWin') {
+      user.wins += 1;
+      user.blackjackWins += 1;
+      user.totalGamesPlayed += 1;
+      user.exp += 200;
+      user.wl = user.wins / user.loses;
+      user.coins += 200;
+    } else if (action === 'rouletteWin') {
+      user.wins += 1;
+      user.rouletteWins += 1;
+      user.totalGamesPlayed += 1;
+      user.exp += 200;
+      user.wl = user.wins / user.loses;
+      user.coins += 200;
+    } else if (action === 'spadesWin') {
+      user.wins += 1;
+      user.spadesWins += 1;
+      user.totalGamesPlayed += 1;
+      user.exp += 200;
+      user.wl = user.wins / user.loses;
+      user.coins += 200;
+    } else if (action === 'unoWin') {
+      user.wins += 1;
+      user.unoWins += 1;
+      user.totalGamesPlayed += 1;
+      user.exp += 200;
+      user.wl = user.wins / user.loses;
+      user.coins += 200;
+    } else if (action === 'lose') {
+      user.loses += 1;
+      user.totalGamesPlayed += 1;
+      user.level > 1 ? user.exp -= 100 : null;
+      user.wl = user.wins / user.loses;
+      user.coins -= 200;
+    }
 
     if (user.coins > user.maxCoins) {
       user.maxCoins = user.coins
       await user.save()
     }
 
+    await user.save();
+
     return new NextResponse(
-      JSON.stringify({ message: 'Maximum coins updated' }),
+      JSON.stringify({ message: 'User stats updated' }),
       { status: 200, headers: { 'Content-Type': 'application/json' } }
-    )
+    );
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return new NextResponse(
       JSON.stringify({ message: 'Internal Server Error' }),
       { status: 500, headers: { 'Content-Type': 'application/json' } }
-    )
+    );
   }
-}
+};
 
 // Example function to update the maximum coins
 
