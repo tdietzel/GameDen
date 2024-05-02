ab76ba7d349c90814336d1c90772e53c39cb63cf
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-05-02 15:59:33 -0700

    add: form to create groups

--- solution/app/components/CreateGroupForm.jsx
+++ solution/app/components/CreateGroupForm.jsx
@@ -22,40 +22,45 @@ const CreateGroupForm = ({ userId }) => {
   };
 
   return (
-    <div className="bg-gray-800 p-8 rounded-lg shadow-lg flex justify-center">
-      <form onSubmit={handleSubmit} className="flex flex-col space-y-4 w-5/6 margin-auto">
-        <input
-          type='text'
-          placeholder='Group Name'
-          value={ name }
-          onChange={(e) => setName(e.target.value)}
-          className="p-2 rounded-lg bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-        />
-        <textarea
-          placeholder='Description'
-          value={ description }
-          onChange={(e) => setDescription(e.target.value)}
-          className="p-2 rounded-lg bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-          rows='3'
-        />
-        <label className="text-white">
+    <>
+      <h1 className="text-5xl text-center font-bold text-white pt-5">
+        Start a group
+      </h1>
+      <div className="bg-gray-800 p-8 rounded-lg shadow-lg flex justify-center">
+        <form onSubmit={handleSubmit} className="flex flex-col space-y-4 w-5/6 margin-auto">
           <input
-            type='radio'
-            value='public'
-            checked={ !isPublic }
-            onChange={() => setIsPublic(false)}
-            className="mr-2"
+            type='text'
+            placeholder='Group Name'
+            value={ name }
+            onChange={(e) => setName(e.target.value)}
+            className="p-2 rounded-lg bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
           />
-          Private
-        </label>
-        <button
-          type='submit'
-          className="px-4 py-2 bg-gradient-to-r from-blue-600 to-blue-800 text-white font-bold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-colors duration-300"
-        >
-          Create Group
-        </button>
-      </form>
-    </div>
+          <textarea
+            placeholder='Description'
+            value={ description }
+            onChange={(e) => setDescription(e.target.value)}
+            className="p-2 rounded-lg bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+            rows='3'
+          />
+          <label className="text-white">
+            <input
+              type='checkbox'
+              value='public'
+              checked={ !isPublic }
+              onChange={() => setIsPublic(false)}
+              className="mr-2"
+            />
+            Private
+          </label>
+          <button
+            type='submit'
+            className="px-4 py-2 bg-gradient-to-r from-blue-600 to-blue-800 text-white font-bold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-colors duration-300"
+          >
+            Create Group
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
